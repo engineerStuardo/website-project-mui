943586f29e8ec5ab22b7dce2373b3e0850fc18fb
engineerStuardo <ingeniero.stuardo@gmail.com>
2021-12-27 07:37:21 -0600

    working on styling and learning a bit of responsiveness in MUI NavigationBar

--- src/NavigationBar.js
+++ src/NavigationBar.js
@@ -1,15 +1,45 @@
-import { Container, Toolbar, Typography, Box, Link } from '@material-ui/core';
+import {
+  Container,
+  Toolbar,
+  Typography,
+  Box,
+  Link,
+  makeStyles,
+} from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+  menuOption: {
+    padding: theme.spacing(1),
+    [theme.breakpoints.up('md')]: {
+      paddingLeft: theme.spacing(3),
+    },
+  },
+  menuBox: {
+    display: 'flex',
+    flexDirection: 'column',
+    [theme.breakpoints.up('md')]: {
+      flexDirection: 'row',
+    },
+  },
+}));
 
 const menu = ['home', 'courses', 'sign up'];
 
 const NavigationBar = () => {
+  const classes = useStyles();
+
   return (
     <Container>
       <Toolbar>
         <Typography>Mammoth Interactive</Typography>
-        <Box>
+        <Box className={classes.menuBox}>
           {menu.map(menuOption => (
-            <Link component='button' variant='body1'>
+            <Link
+              key={menuOption}
+              component='button'
+              variant='body1'
+              className={classes.menuOption}
+            >
               {menuOption.toUpperCase()}
             </Link>
           ))}
